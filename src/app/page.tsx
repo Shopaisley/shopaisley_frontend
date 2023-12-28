c608bb03209d1c63ea811efed990789f4c3fc349
oge1ata <yomacorp54@gmail.com>
2023-12-28 21:05:51 +0100

    Working on view page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+// @ts-ignore
+// use client
+
 import Image from 'next/image'
 import styles from './page.module.css'
 import CategoryProduct from '@/components/CategoryProduct'
 import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import ProductBanner from '@/components/ProductBanner'
+import ProductPage from '@/components/ViewPage'
+
 
 
 export default function Home() {
@@ -9,8 +16,13 @@ export default function Home() {
     <main>
       {/* < CategoryProduct/> */}
 
-      <Footer/>
- 
+      {/* <Footer/> */}
+
+      {/* <Header />
+      <ProductBanner categoryName="Electronics" categoryText="Explore the latest gadgets" />
+  */}
+    <ProductPage productCompany='Apple' productDesc='Super Retina XDR display
+6.1-inch (diagonal) all-screen OLED display' productName='iPhone 15' productPrice='NGN 1,050,000'/>
     </main>
   )
 }
