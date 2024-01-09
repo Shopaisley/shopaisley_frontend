c68a8b90f975b2a49d6d05c89eb18df022518713
jeffreyibezim <153927117+jeffreyibezim@users.noreply.github.com>
2024-01-09 22:02:07 +0100

    Add files via upload
    This is the component for the iphone box in the product grid page

---
+++ iphone box component 2.tsx
@@ -0,0 +1,68 @@
+import type { NextPage } from 'next';
+
+import { Box, ChakraProvider } from '@chakra-ui/react';
+
+
+const ProductGrid:NextPage = () => {
+  	return (
+      <ChakraProvider>
+
+
+
+<Box pos = {"relative"}
+  height= "136.8px"
+  width=" 132.69px"
+  background= "#c4c4c4"
+ />
+
+<Box  pos = {"relative"}
+  letter-spacing = "-0.57px"
+
+ 
+  fontWeight={"bold"}
+  left={"5px"}
+  
+  top={"9px"}>
+	
+	
+	iPhone 15</Box>
+
+
+
+<Box pos = {"relative"}
+  letter-spacing = "-0.57px"
+  line-height = "30.86px"
+  left={"5px"}
+  top={"8px"}
+  
+  >NGN 1,050,000</Box>
+
+
+
+<Box   position= "relative"
+  font-size=" 14.4px"
+
+  letter-spacing= "-0.41px"
+  line-height= "20.57px"
+  left={"149px"}
+  top={"-39px"}
+ 
+
+
+  >Apple</Box>
+
+
+
+
+
+
+
+
+        </ChakraProvider>
+
+
+
+        );
+};
+
+export default ProductGrid;
