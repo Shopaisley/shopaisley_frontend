b0b98d1ba0f0d7244502127dd3bec5a3e2284e78
Solexi <sophia.ezeh@pau.edu.ng>
2024-01-11 15:56:03 +0100

    "Edited the Header and Footer Components"

--- src/app/product-catalogue/page.tsx
+++ src/app/product-catalogue/page.tsx
@@ -1,29 +1,20 @@
 import { Box } from "@chakra-ui/react";
 import CategoryBox from "@/components/CategoryBox";
 import acImage from "@/assets/images/electronics/air-conditioner.jpeg";
+import ProductBanner from "@/components/ProductBanner";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const Page = () => {
   return (
     <Box>
-      <CategoryBox categoryName={"Nike"} categoryImage={acImage} />
+      <Header/>
+      <Footer />
+      {/* <CategoryBox categoryName={"Nike"} categoryImage={acImage} /> */}
+      {/* <ProductBanner categoryName={"Apple"} categoryText={"Apple Iphone 15"} /> */}
     </Box>
   );
 };
 
 export default Page;
-
-// import CategoryBox from "@/components/CategoryBox"
-// import { Box } from "@chakra-ui/react"
-// import acImage from "../../../public/assets/images/electronics/air-conditioner.jpeg";
-// // import ac from "../../../public/next.svg";
-
-// const Page = () => {
-//     return (
-//         <Box>
-//             <CategoryBox categoryName={"Nike"} categoryImage={"/air-conditioner.jpeg"}/>
-//         </Box>
-//     )
-// }
-
-// export default Page
 
--- src/components/ViewPage/index.tsx
+++ src/components/ViewPage/index.tsx
@@ -1,4 +1,3 @@
-// @ts-ignore
 "use client";
 
 import React, { useState, FC } from "react";
