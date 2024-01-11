003b5500ee9b3cf3ab5f15a0e71ea8a6a1a26297
Oyindamola J. Lewu <oyindamola.lewu@pau.edu.ng>
2024-01-11 13:25:37 +0100

    creation of main components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { fonts } from './fonts'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,14 +10,20 @@ export const metadata: Metadata = {
   description: 'Anything you can buy',
 }
 
+import { Providers } from './providers'
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode,
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en' className={fonts.rubik.variable}>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 }
+
+
