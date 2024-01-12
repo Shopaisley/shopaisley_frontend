d75a5513b58fa2bc570596398ed31955ad116132
Solexi <sophia.ezeh@pau.edu.ng>
2024-01-12 09:50:41 +0100

    "Added Checkout Folders"

--- src/app/product-catalogue/page.tsx
+++ src/app/product-catalogue/page.tsx
@@ -4,12 +4,14 @@ import acImage from "@/assets/images/electronics/air-conditioner.jpeg";
 import ProductBanner from "@/components/ProductBanner";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import ProductBox from "@/components/SingleProduct";
 
 const Page = () => {
   return (
     <Box>
       <Header/>
       <Footer />
+      {/* <ProductBox productName={"Iphone 15"} productPrice={"1500"} ProductMaker={"Apple"} /> */}
       {/* <CategoryBox categoryName={"Nike"} categoryImage={acImage} /> */}
       {/* <ProductBanner categoryName={"Apple"} categoryText={"Apple Iphone 15"} /> */}
     </Box>
