8a6479454048f30ac5d97922fc78e47156a3ad79
Abba00 <aaliconcern@gmail.com>
2024-02-03 03:06:56 +0100

    Route Added

--- sellers/src/App.tsx
+++ sellers/src/App.tsx
@@ -1,10 +1,16 @@
-// import './App.css'
-
+import './App.css'
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { Login } from './Routes';
 
 
 
 function App() {
   return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
